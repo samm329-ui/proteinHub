2892cc5715bb8e7d62cc79830345bf653265222b
Jishu Das <jishudas329@gmail.com>
2026-01-12 09:54:49 +0000

    2026-01-12T08:26:49Z [web] <Firebase Studio> Starting preview... 2026-01

--- next.config.ts
+++ next.config.ts
@@ -28,8 +28,26 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'xizgjjkyqpzyuwxcgcuk.supabase.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
+  devIndicators: {
+    buildActivity: false,
+  },
+  // Adding this to allow cross-origin requests in development
+  // This is necessary for the Firebase Studio environment
+  ...(process.env.NODE_ENV === 'development' && {
+    experimental: {
+      allowedDevOrigins: [
+        'https://*.cluster-cz5nqyh5nreq6ua6gaqd7okl7o.cloudworkstations.dev',
+      ],
+    },
+  }),
 };
 
 export default nextConfig;
